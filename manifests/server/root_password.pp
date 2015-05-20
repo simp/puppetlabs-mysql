ededb01eaa98112d0f569c4a3e6b8dc6c3f15902
tphoney <tp@puppetlabs.com>
2015-05-20 12:05:29 +0100

    dont print diff root password in diff

--- manifests/server/root_password.pp
+++ manifests/server/root_password.pp
@@ -13,9 +13,10 @@ class mysql::server::root_password {
 
   if $mysql::server::create_root_my_cnf == true and $mysql::server::root_password != 'UNSET' {
     file { "${::root_home}/.my.cnf":
-      content => template('mysql/my.cnf.pass.erb'),
-      owner   => 'root',
-      mode    => '0600',
+      content   => template('mysql/my.cnf.pass.erb'),
+      owner     => 'root',
+      mode      => '0600',
+      show_diff => false,
     }
     if $mysql::server::create_root_user == true {
       Mysql_user['root@localhost'] -> File["${::root_home}/.my.cnf"]
